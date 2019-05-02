6c4d293cbeab590cd86a975a45e799dce36a6c94
HuseyinAltnsk <huseyinaltnskcsc@gmail.com>
2019-05-02 17:16:03 -0400

    fetchAPi.js now gets student data

--- fetchApi.js
+++ fetchApi.js
@@ -1,7 +1,7 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
 const fetch = require("node-fetch");
-fetch("https://elastic.snaplogic.com:443/api/1/rest/slsched/feed/davidsonDEV/Sandbox-NickRoberts/HR_Employee_Directory_API/EmployeeDirectory", {
+/*fetch("https://elastic.snaplogic.com:443/api/1/rest/slsched/feed/davidsonDEV/Sandbox-NickRoberts/HR_Employee_Directory_API/EmployeeDirectory", {
   headers: { "Content-Type": "application/json", "Authorization": "Bearer 6PHSMY0BSAdRzLruLEVRc1A3FnnjxIjA" },
   credentials: "include"
   })
@@ -16,40 +16,48 @@ fetch("https://elastic.snaplogic.com:443/api/1/rest/slsched/feed/davidsonDEV/San
       if (err) throw err;
       var DD = db.db("DavidsonDirectory");
 
-      // Create collections
-      DD.createCollection("members", function(err, res) {
-        if (err) throw err;
-        console.log("Collection 'members' created!");
-      });
-      DD.createCollection("professors", function(err, res) {
-        if (err) throw err;
-        console.log("Collection 'professors' created!");
-      });
-      
-      // Add the person objects one by one depending on their job titles
-      myJson.forEach(personObj => {
-        if(personObj.job_title == "Professor"){
+      DD.fs.drop()
 
-          // add professors into the 'professors' collection
-          DD.collection("professors").insertOne(personObj, function(err, res) {
-            if (err) throw err;
-            console.log("Professor added into the database.");
-            db.close();
-          });
-        }else{
-          // add other members into the 'members' collection
-          DD.collection("members").insertOne(personObj, function(err, res) {
-            if (err) throw err;
-            console.log("Member added into the database.");
-            db.close();
-          });
-        }
-      });
-      /*DD.collection("members").insertMany(myJson, function(err, res) {
+      // Create collection
+      DD.createCollection("fs", function(err, res) {
         if (err) throw err;
-        console.log("All documents are successfully inserted into the database.");
+        console.log("Collection 'Faculty/Staff' created!");
+      });
+      // add faculty/staff into the fs collection of DD database
+      DD.collection("fs").insertMany(myJson, function(err, res) {
+        if (err) throw err;
+        console.log("Faculty/Staff added into the database.");
         db.close();
-      });*/
+      });
+    });
+});*/
+fetch("https://elastic.snaplogic.com/api/1/rest/slsched/feed/davidsonDEV/Sandbox-NickRoberts/ST_Student_Directory_API/StudentDirectory", {
+  headers: { "Content-Type": "application/json", "Authorization": "Bearer n7V44wXBiIdkXkk5Pv6NAIIOKLJ0JE75" },
+  credentials: "include"
+  })
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    data = JSON.stringify(myJson)
+    console.log(data.length);
+    // Connect to the mongo client after running "sudo mongod" on Terminal
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var DD = db.db("DavidsonDirectory");
+
+      
+
+      // Create collection
+      DD.createCollection("st", function(err, res) {
+        if (err) throw err;
+        console.log("Collection 'Student' created!");
+      });
+      // add students into the st collection of DD database
+      DD.collection("st").insertMany(myJson, function(err, res) {
+        if (err) throw err;
+        console.log("Student added into the database.");
+        db.close();
+      });
     });
 });
-
